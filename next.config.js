aa85858e0f46345f94e252139bb3d20cc49ffd2a
ryota-murakami <dojce1048@gmail.com>
2023-10-07 09:34:26 +0900

    Update next.config.js with recommended settings for pages directory

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Recommended for the `pages` directory, default in `app`.
-  experimental: {
-    appDir: true,
-    serverActions: true,
-  },
 }
 module.exports = nextConfig
